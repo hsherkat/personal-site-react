c91e32ab03bbe0ae732983a638c1d2c1319cd2c9
Hooman Sherkat <3044447+hsherkat@users.noreply.github.com>
2022-10-19 13:10:24 -0700

    deleted initial files and made simple component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <header> Header component! </header>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default Header;
